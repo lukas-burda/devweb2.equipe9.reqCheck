47a6196c0db6270a534a165dd8cdc0ac0305139e
Pedro Henrique <pedroponto788@gmail.com>
2020-10-04 22:16:00 -0300

    Criacao component index

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
+import { ListProdutoComponent } from './views/list-produto/list-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: IndexComponent
+},
+{
+  path: 'produto/lista',
+  component: ListProdutoComponent
+}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './views/index/index.component';
+import { ListProdutoComponent } from './views/list-produto/list-produto.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ListProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
